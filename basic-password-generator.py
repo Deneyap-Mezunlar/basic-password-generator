7d7846e8e1afebe66ca019e24a88b6389680fa52
Ahmet Efe AKYAZI <46342237+efe3535@users.noreply.github.com>
2022-01-30 02:23:52 +0300

    Add argument support, which means PIPE support.
    Hint: A pipe is that `|` in `echo "15" | python3 basic-password-generator.py`.
    Summary, TL;DR:
    * Added error handling
    * Added argument support

--- basic-password-generator.py
+++ basic-password-generator.py
@@ -1,9 +1,24 @@
 import random
 import string
+from sys import argv
 
 print("Merhaba! SeoLeX'in şifre üreticine hoşgeldin.")
 
-length = int(input("\nOluşturmak istediğin şifrenin karakter sayısını gir! \n"))
+length = int()
+
+if len(argv) < 2: # Argument not specified / Arguman belirtilmemisse
+    try:
+        length = int(input("\nOluşturmak istediğin şifrenin karakter sayısını gir! \n"))
+    except ValueError:
+        print("Sadece sayi girmelisiniz.")
+        exit(1)
+else:
+    try:
+        length = int(argv[1])
+    except ValueError:
+        print("Sadece sayi girmelisiniz.")
+        exit(1)
+
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 number = string.digits
